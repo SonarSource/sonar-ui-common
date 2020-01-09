1f4c398e3315599faaff766d52a785ec76ad5a47
Mathieu Suen <mathieu.suen@sonarsource.com>
2020-01-09 09:44:20 +0100

    SONAR-12860: Add custom type for RadioToggle and fix size glitch.

--- components/controls/__tests__/RadioToggle-test.tsx
+++ components/controls/__tests__/RadioToggle-test.tsx
@@ -23,33 +23,72 @@ import { change } from '../../../helpers/testUtils';
 import RadioToggle from '../RadioToggle';
 
 it('renders', () => {
-  expect(shallow(getSample())).toMatchSnapshot();
+  expect(shallowRender()).toMatchSnapshot();
 });
 
 it('calls onCheck', () => {
   const onCheck = jest.fn();
-  const wrapper = shallow(getSample({ onCheck }));
-  change(wrapper.find('input[value="two"]'), 'two');
+  const wrapper = shallowRender({ onCheck });
+  change(wrapper.find('input[id="sample__two"]'), '');
   expect(onCheck).toBeCalledWith('two');
+});
+
+it('handles numeric values', () => {
+  const onCheck = jest.fn();
+  const wrapper = shallowRender({
+    onCheck,
+    options: [
+      { value: 1, label: 'first', tooltip: 'foo' },
+      { value: 2, label: 'second', tooltip: 'bar' }
+    ],
+    value: 1
+  });
+  change(wrapper.find('input[id="sample__2"]'), '');
+  expect(onCheck).toBeCalledWith(2);
+});
+
+it('handles boolean values', () => {
+  const onCheck = jest.fn();
+  const wrapper = shallowRender({
+    onCheck,
+    options: [
+      { value: true, label: 'yes', tooltip: 'foo' },
+      { value: false, label: 'no', tooltip: 'bar' }
+    ],
+    value: true
+  });
+  change(wrapper.find('input[id="sample__false"]'), '');
+  expect(onCheck).toBeCalledWith(false);
+});
+
+it('initialize value', () => {
+  const onCheck = jest.fn();
+  const wrapper = shallowRender({
+    onCheck,
+    options: [
+      { value: 1, label: 'first', tooltip: 'foo' },
+      { value: 2, label: 'second', tooltip: 'bar', disabled: true }
+    ],
+    value: 2
+  });
+  expect(wrapper.find('input[checked=true]').prop('id')).toBe('sample__2');
 });
 
 it('accepts advanced options fields', () => {
   expect(
-    shallow(
-      getSample({
-        options: [
-          { value: 'one', label: 'first', tooltip: 'foo' },
-          { value: 'two', label: 'second', tooltip: 'bar', disabled: true }
-        ]
-      })
-    )
+    shallowRender({
+      options: [
+        { value: 'one', label: 'first', tooltip: 'foo' },
+        { value: 'two', label: 'second', tooltip: 'bar', disabled: true }
+      ]
+    })
   ).toMatchSnapshot();
 });
 
-function getSample(props?: any) {
+function shallowRender(props?: Partial<RadioToggle['props']>) {
   const options = [
     { value: 'one', label: 'first' },
     { value: 'two', label: 'second' }
   ];
-  return <RadioToggle name="sample" onCheck={() => true} options={options} {...props} />;
+  return shallow(<RadioToggle name="sample" onCheck={() => true} options={options} {...props} />);
 }
